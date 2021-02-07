0733fc98ff32ce6a51cd5c75b80a4ccc2b70b68f
Adi Nugroho <addieze@ymail.com>
2021-02-07 19:33:45 +0700

    add head

---
+++ components/CustomHead.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+
+const CustomHead = () => {
+  return (
+    <Head>
+      <title>Dilamar - Nehan Dev</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="shortcut icon" href="/images/icon.png" type="image/png"></link>
+    </Head>
+  );
+};
+
+export default CustomHead;
