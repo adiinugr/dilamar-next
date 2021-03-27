62473bb16d3f9dcd54b735f7659ef2df5b6d014c
Adi Nugroho <addieze@ymail.com>
2021-03-27 12:33:51 +0700

    fix clientheader3

--- pages/client/components/clientHeader.js
+++ pages/client/components/clientHeader.js
@@ -1,14 +1,16 @@
 import Link from "next/link";
 
 const ClientHeader = ({ namaClient }) => {
-  const nama = namaClient.replace("-", " & ").toUpperCase();
+  const nama = namaClient || "Nama Client";
+
+  const formatedNama = nama.replace("-", " & ").toUpperCase();
 
   return (
     <section className="bg-green-600 text-gray-50 px-4 md:px-36 fixed z-20 w-screen">
       <div className="flex justify-between items-center py-4">
         <div className="font-display text-3xl">Nehan Dev</div>
         <div className="flex items-center">
-          <div className="mr-4">{nama}</div>
+          <div className="mr-4">{formatedNama}</div>
           {/* <div className="cursor-pointer bg-green-700 px-4 py-1 rounded-sm">
             Logout
           </div> */}
