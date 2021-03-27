602ca01504cb2eb43e56b205af6de7d6473b191f
Adi Nugroho <addieze@ymail.com>
2021-03-28 06:06:15 +0700

    kharnisa add video

--- components/invitation/Gallery.js
+++ components/invitation/Gallery.js
@@ -1,5 +1,40 @@
 import Image from "next/image";
 import React from "react";
+import YouTube from "react-youtube";
+import YoutubePlayer from "../YoutubePlayer";
+
+const galleryData = [
+  {
+    id: 1,
+    type: "image",
+    src: "/kharnisa-imam/couple.png",
+  },
+  {
+    id: 2,
+    type: "image",
+    src: "/kharnisa-imam/couple2.png",
+  },
+  {
+    id: 3,
+    type: "image",
+    src: "/kharnisa-imam/couple3.png",
+  },
+  {
+    id: 4,
+    type: "image",
+    src: "/kharnisa-imam/couple4.png",
+  },
+  {
+    id: 5,
+    type: "image",
+    src: "/kharnisa-imam/couple5.png",
+  },
+  {
+    id: 6,
+    type: "video",
+    videoId: "n9uIqjzZS_4",
+  },
+];
 
 export const GalleryOne = () => {
   return (
@@ -11,61 +46,33 @@ export const GalleryOne = () => {
         Gallery
       </div>
       <div className="text-center my-6">#MenjadiImamnyaNisa</div>
-      <div
-        data-aos="fade-up"
-        className="w-5/6 md:w-2/3 h-96 relative mx-auto mb-4"
-      >
-        <Image
-          src="/kharnisa-imam/couple.png"
-          layout="fill"
-          objectFit="cover"
-          className="absolute"
-        />
-      </div>
-      <div
-        data-aos="fade-up"
-        className="w-5/6 md:w-2/3 h-96 relative mx-auto mb-4"
-      >
-        <Image
-          src="/kharnisa-imam/couple2.png"
-          layout="fill"
-          objectFit="cover"
-          className="absolute"
-        />
-      </div>
-      <div
-        data-aos="fade-up"
-        className="w-5/6 md:w-2/3 h-96 relative mx-auto mb-4"
-      >
-        <Image
-          src="/kharnisa-imam/couple3.png"
-          layout="fill"
-          objectFit="cover"
-          className="absolute"
-        />
-      </div>
-      <div
-        data-aos="fade-up"
-        className="w-5/6 md:w-2/3 h-96 relative mx-auto mb-4"
-      >
-        <Image
-          src="/kharnisa-imam/couple4.png"
-          layout="fill"
-          objectFit="cover"
-          className="absolute"
-        />
-      </div>
-      <div
-        data-aos="fade-up"
-        className="w-5/6 md:w-2/3 h-96 relative mx-auto mb-4"
-      >
-        <Image
-          src="/kharnisa-imam/couple5.png"
-          layout="fill"
-          objectFit="cover"
-          className="absolute"
-        />
-      </div>
+
+      {galleryData.map((gallery) => {
+        if (gallery.type === "image") {
+          return (
+            <div
+              data-aos="fade-up"
+              className="w-5/6 md:w-2/3 h-96 relative mx-auto mb-4"
+            >
+              <Image
+                src={gallery.src}
+                layout="fill"
+                objectFit="cover"
+                className="absolute"
+              />
+            </div>
+          );
+        } else if (gallery.type === "video") {
+          return (
+            <div
+              data-aos="fade-up"
+              className="w-5/6 md:w-2/3 h-96 relative mx-auto mb-4"
+            >
+              <YoutubePlayer videoId={gallery.videoId} />
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
